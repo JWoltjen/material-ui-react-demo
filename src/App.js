616a27f872bd25b700e661c73595f7f357ca8ea4
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-01-12 13:04:24 -0600

    complete iteration1

--- src/App.js
+++ src/App.js
@@ -98,11 +98,11 @@ function App() {
       <ButtonStyled/>
 
     <Grid container spacing={2} justify="center">
-      <Grid item xs={12} sm={6}>
-        <Paper style={{height: 75, width: '100%', }}/> 
+      <Grid item>
+        <Paper style={{height: 75, width: 50, }}/> 
       </Grid>
-      <Grid item xl={3}>
-        <Paper style={{height: 75, width: '100%', }}/> 
+      <Grid item>
+        <Paper style={{height: 75, width: 50, }}/> 
       </Grid>
       <Grid item>
         <Paper style={{height: 75, width: 50, }}/> 
