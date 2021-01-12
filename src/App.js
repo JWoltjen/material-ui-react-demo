03ce796fea1765d43d9dbcde68eefa432d8e0b5d
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-01-11 22:37:35 -0600

    Add discard button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 
 import './App.css';
 import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
 import SaveIcon from '@material-ui/icons/Save'
 
 function App() {
   return (
     <div className="App">
+      <ButtonGroup>
       <Button 
         endIcon={<SaveIcon />}
         size='large'
         variant='contained'
         color='primary'>
-          Hello World
+          Save
       </Button>
+      <Button 
+        endIcon={<SaveIcon />}
+        size='large'
+        variant='contained'
+        color='secondary'>
+          Discard
+      </Button>
+      </ButtonGroup>
     </div>
   );
 }
