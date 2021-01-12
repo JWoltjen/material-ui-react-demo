9d5fb92c164698874ad7ad9abec11221ab0d232c
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-01-11 22:45:57 -0600

    add checkbox example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-
+import React from 'react'
 import './App.css';
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Checkbox from '@material-ui/core/Checkbox'
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true)
+  return (
+    <div>
+     <Checkbox 
+     checked={checked}
+     onChange={(e)=>setChecked(e.target.checked)}
+     inputProps={{
+       'aria-label': 'secondary checkbox'
+     }}
+     />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <ButtonGroup>
+      <ButtonGroup size='large' variant='contained' color='primary' >
       <Button 
         endIcon={<SaveIcon />}
-        size='large'
-        variant='contained'
-        color='primary'>
+      >
           Save
       </Button>
       <Button 
-        endIcon={<SaveIcon />}
-        size='large'
-        variant='contained'
-        color='secondary'>
+        endIcon={<DeleteIcon />}
+        >
           Discard
       </Button>
       </ButtonGroup>
